fe041f8c8322fe0c7084c9dbfe2d4b295c519bc2
Andrew Jones <joneseyit@protonmail.com>
2020-01-16 16:38:04 -0600

    setup basic framework

---
+++ portfolio-app/pages/about.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+// const About = () => {
+//     const message = "Hey let get the heck outta town"
+//     return (
+        
+//         React.createElement('h2', null, "Can you swim")
+//     )
+// }
+
+class About extends React.Component {
+
+    render (){
+        return (
+            <div>
+                <h2>Puh-leeaase
+                    
+                </h2>
+                <h1>Hey I made a comp from classes </h1>
+            </div>
+
+        )
+        
+    }
+}
+
+export default About;
