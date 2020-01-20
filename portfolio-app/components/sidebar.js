d72786444c8a50b0563641a0ac896bc36476c5fa
Andrew Jones <joneseyit@protonmail.com>
2020-01-20 17:03:24 -0600

    refactored - put in faux data

--- portfolio-app/components/sidebar.js
+++ portfolio-app/components/sidebar.js
@@ -1,9 +1,12 @@
-const SideBar = () => {
+import { useState } from 'react'
+
+const SideBar = (props) => {
+   
+
     return (
 
-             <div className="col-lg-3">
-
-            <h1 className="my-4">Shop Name</h1>
+        <div className="col-lg-3">
+            <h1 className="my-4">{props.appName}</h1>
             <div className="list-group">
             <a href="#" className="list-group-item">Category 1</a>
             <a href="#" className="list-group-item">Category 2</a>
